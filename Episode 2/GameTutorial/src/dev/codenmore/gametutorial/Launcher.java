0e6949ebea555c5e7f2d7f7c92633e4436f4a82f
CodeNMore <jackofall55@gmail.com>
2014-11-12 17:26:24 -0500

    Episode 2 Source Code

---
+++ Episode 2/GameTutorial/src/dev/codenmore/gametutorial/Launcher.java
@@ -0,0 +1,11 @@
+package dev.codenmore.gametutorial;
+
+import dev.codenmore.gametutorial.display.Display;
+
+public class Launcher {
+
+	public static void main(String[] args){
+		new Display(720, 480, "Title!");
+	}
+	
+}
