ed110430310db34df24a76e3f105c4ea5a775dba
CodeNMore <jackofall55@gmail.com>
2014-12-24 14:13:32 -0500

    Episode 8 Source Added

---
+++ Episode 8/TileGame/src/dev/codenmore/tilegame/Game.java
@@ -0,0 +1,103 @@
+package dev.codenmore.tilegame;
+
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+import dev.codenmore.tilegame.display.Display;
+import dev.codenmore.tilegame.gfx.ImageLoader;
+import dev.codenmore.tilegame.gfx.SpriteSheet;
+
+public class Game implements Runnable {
+
+	private Display display;
+	public int width, height;
+	public String title;
+	
+	private boolean running = false;
+	private Thread thread;
+	
+	private BufferStrategy bs;
+	private Graphics g;
+	
+	private BufferedImage test;
+	private SpriteSheet sheet;
+	
+	public Game(String title, int width, int height){
+		this.width = width;
+		this.height = height;
+		this.title = title;
+	}
+	
+	private void init(){
+		display = new Display(title, width, height);
+		test = ImageLoader.loadImage("/textures/sheet.png");
+		sheet = new SpriteSheet(test);
+	}
+	
+	private void tick(){
+		
+	}
+	
+	private void render(){
+		bs = display.getCanvas().getBufferStrategy();
+		if(bs == null){
+			display.getCanvas().createBufferStrategy(3);
+			return;
+		}
+		g = bs.getDrawGraphics();
+		//Clear Screen
+		g.clearRect(0, 0, width, height);
+		//Draw Here!
+		
+		g.drawImage(sheet.crop(32, 0, 32, 32), 5, 5, null);
+		
+		//End Drawing!
+		bs.show();
+		g.dispose();
+	}
+	
+	public void run(){
+		
+		init();
+		
+		while(running){
+			tick();
+			render();
+		}
+		
+		stop();
+		
+	}
+	
+	public synchronized void start(){
+		if(running)
+			return;
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	public synchronized void stop(){
+		if(!running)
+			return;
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
