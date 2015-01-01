c0860bc571dc6e96f22d8d136d963b1203e30576
CodeNMore <jackofall55@gmail.com>
2015-01-01 09:18:18 -0500

    Episode 10 Source Added

---
+++ Episode 10/TileGame/src/dev/codenmore/tilegame/gfx/Assets.java
@@ -0,0 +1,21 @@
+package dev.codenmore.tilegame.gfx;
+
+import java.awt.image.BufferedImage;
+
+public class Assets {
+	
+	private static final int width = 32, height = 32;
+	
+	public static BufferedImage player, dirt, grass, stone, tree;
+
+	public static void init(){
+		SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/sheet.png"));
+		
+		player = sheet.crop(0, 0, width, height);
+		dirt = sheet.crop(width, 0, width, height);
+		grass = sheet.crop(width * 2, 0, width, height);
+		stone = sheet.crop(width * 3, 0, width, height);
+		tree = sheet.crop(0, height, width, height);
+	}
+	
+}
