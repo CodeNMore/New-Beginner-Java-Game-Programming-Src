1aeb746f9551510dba42be894d467904d6e78591
CodeNMore <jackofall55@gmail.com>
2015-02-24 11:09:37 -0500

    Episode 16 Source Added

---
+++ Episode 16/TileGame/src/dev/codenmore/tilegame/entities/creatures/Creature.java
@@ -0,0 +1,63 @@
+package dev.codenmore.tilegame.entities.creatures;
+
+import dev.codenmore.tilegame.entities.Entity;
+
+public abstract class Creature extends Entity {
+	
+	public static final int DEFAULT_HEALTH = 10;
+	public static final float DEFAULT_SPEED = 3.0f;
+	public static final int DEFAULT_CREATURE_WIDTH = 64,
+							DEFAULT_CREATURE_HEIGHT = 64;
+	
+	protected int health;
+	protected float speed;
+	protected float xMove, yMove;
+
+	public Creature(float x, float y, int width, int height) {
+		super(x, y, width, height);
+		health = DEFAULT_HEALTH;
+		speed = DEFAULT_SPEED;
+		xMove = 0;
+		yMove = 0;
+	}
+	
+	public void move(){
+		x += xMove;
+		y += yMove;
+	}
+	
+	//GETTERS SETTERS
+
+	public float getxMove() {
+		return xMove;
+	}
+
+	public void setxMove(float xMove) {
+		this.xMove = xMove;
+	}
+
+	public float getyMove() {
+		return yMove;
+	}
+
+	public void setyMove(float yMove) {
+		this.yMove = yMove;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public float getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+	
+}
---
+++ Episode 16/TileGame/src/dev/codenmore/tilegame/states/GameState.java
@@ -0,0 +1,27 @@
+package dev.codenmore.tilegame.states;
+
+import java.awt.Graphics;
+
+import dev.codenmore.tilegame.Game;
+import dev.codenmore.tilegame.entities.creatures.Player;
+
+public class GameState extends State {
+	
+	private Player player;
+	
+	public GameState(Game game){
+		super(game);
+		player = new Player(game, 100, 100);
+	}
+	
+	@Override
+	public void tick() {
+		player.tick();
+	}
+
+	@Override
+	public void render(Graphics g) {
+		player.render(g);
+	}
+
+}
