c85943076b0a856f3386b8c2db69428c525c42ba
CodeNMore <jackofall55@gmail.com>
2015-06-23 14:16:06 -0400

    Episode 22 Source Added

---
+++ Episode 22/TileGame/src/dev/codenmore/tilegame/tiles/GrassTile.java
@@ -0,0 +1,11 @@
+package dev.codenmore.tilegame.tiles;
+
+import dev.codenmore.tilegame.gfx.Assets;
+
+public class GrassTile extends Tile {
+
+	public GrassTile(int id) {
+		super(Assets.grass, id);
+	}
+
+}
